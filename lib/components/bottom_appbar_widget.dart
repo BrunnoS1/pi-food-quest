0f747d1f2f64dfb3b60923a89d7a5cc95dac71bf
Pedro Gabriel <pedrogmaximo@gmail.com>
2024-11-05 11:05:04 -0300

    Adição de imagens (Logo, IMT); Mudança em detalhes login, cadastro e botões;

--- lib/components/bottom_appbar_widget.dart
+++ lib/components/bottom_appbar_widget.dart
@@ -1,91 +1,98 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class BottomAppBarWidget extends StatefulWidget {
-  const BottomAppBarWidget({super.key});
+  class BottomAppBarWidget extends StatefulWidget {
+    const BottomAppBarWidget({super.key});
 
-  @override
-  State<BottomAppBarWidget> createState() => _BottomAppBarWidgetState();
-}
+    @override
+    State<BottomAppBarWidget> createState() => _BottomAppBarWidgetState();
+  }
 
-class _BottomAppBarWidgetState extends State<BottomAppBarWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      height: 100,
-      color: const Color.fromARGB(0, 123, 167, 150),
-      elevation: 0,
-      child: SizedBox(
-        height: 50,
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Espaçamento entre as seções
-            children: [
-              // Texto "Sobre nós" à esquerda
-              GestureDetector(
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text("Sobre nós"),
-                        content: const Text(
-                          "Somos alunos de Ciência da Computação e criamos este jogo didático sobre o sistema "
-                          "digestório para tornar o aprendizado mais acessível e divertido. Nossa meta é ajudar estudantes a "
-                          "entender melhor o funcionamento do sistema digestório com perguntas interativas e conteúdo "
-                          "educativo. Aproveite o jogo e aprenda de forma divertida!",
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text(
-                              "Fechar",
-                              style: TextStyle(color: Colors.black),
+  class _BottomAppBarWidgetState extends State<BottomAppBarWidget> {
+    @override
+    Widget build(BuildContext context) {
+      return BottomAppBar(
+        height: 100,
+        color: const Color.fromARGB(0, 123, 167, 150),
+        elevation: 0,
+        child: SizedBox(
+          height: 50,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Espaçamento entre as seções
+              children: [
+                
+                // Texto "Sobre nós" à esquerda
+                MouseRegion(
+                  cursor: SystemMouseCursors.click, // Define o cursor como uma mãozinha
+                  child: GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text("Sobre nós"),
+                            content: const Text(
+                              "Somos alunos de Ciência da Computação e criamos este jogo didático sobre o sistema "
+                              "digestório para tornar o aprendizado mais acessível e divertido. Nossa meta é ajudar estudantes a "
+                              "entender melhor o funcionamento do sistema digestório com perguntas interativas e conteúdo "
+                              "educativo. Aproveite o jogo e aprenda de forma divertida!",
                             ),
-                          ),
-                        ],
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  "Fechar",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
                       );
                     },
-                  );
-                },
-                child: const Text(
-                  "Sobre nós",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
+                    child: const Text(
+                      "Sobre nós",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        decorationThickness: 3,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
-              ),
 
-              // Imagens centralizadas no meio da BottomAppBar
-              const Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/logo_piaget.png'),
-                    height:200,
-                    width:80,
-                  ),
-                  SizedBox(width: 8), // Espaçamento entre as imagens
-                  Image(
-                    image: AssetImage('assets/images/logo-IMT.png'),
-                    height:700,
-                    width:150,
-                  ),
-                ],
-              ),
+                // Imagens centralizadas no meio da BottomAppBar
+                const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/logo_piaget.png'),
+                      height: 700,
+                      width: 80,
+                    ),
+                    SizedBox(width: 8), // Espaçamento entre as imagens
+                    Image(
+                      image: AssetImage('assets/images/logo-IMT.png'),
+                      height: 700,
+                      width: 150,
+                    ),
+                  ],
+                ),
 
-              // Placeholder para ocupar o espaço no final e equilibrar a centralização
-              const SizedBox(width: 50),
-            ],
+                // Placeholder para ocupar o espaço no final e equilibrar a centralização
+                const SizedBox(width: 50),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
 
 
--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -17,8 +17,9 @@ class MyButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-          padding: const EdgeInsets.all(25),
-          margin: const EdgeInsets.symmetric(horizontal: 25),
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.symmetric(horizontal: 8),
+          width: 300,
           decoration: BoxDecoration(
               color: const Color.fromRGBO(220, 15, 75, 1), borderRadius: BorderRadius.circular(16)),
           child: Center(
@@ -26,7 +27,7 @@ class MyButton extends StatelessWidget {
                 style: const TextStyle(
                     color: Color.fromRGBO(255, 215, 90, 1),
                     fontWeight: FontWeight.bold,
-                    fontSize: 16)),
+                    fontSize: 18)),
           )),
     );
   }
