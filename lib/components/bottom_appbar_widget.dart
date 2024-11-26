13525099f32a2516047da9bc242cdc9a52a68021
Brunno <129430164+BrunnoS1@users.noreply.github.com>
2024-11-26 16:06:55 -0300

    fix responsividade bottombar

--- lib/components/bottom_appbar_widget.dart
+++ lib/components/bottom_appbar_widget.dart
@@ -23,95 +23,78 @@ class _BottomAppBarWidgetState extends State<BottomAppBarWidget> {
                 MainAxisAlignment.spaceBetween, // Espaçamento entre as seções
             children: [
               // Texto "Sobre nós" à esquerda
-              GestureDetector(
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        backgroundColor: const Color.fromARGB(
-                            255, 75, 75, 75), // Cor de fundo da interface
-                        title: const Text(
-                          "Sobre nós",
-                          style: TextStyle(
-                              color: Colors.white), // Cor do texto do título
-                        ),
-                        content: const Text(
-                          "Somos alunos de Ciência da Computação e criamos este jogo didático sobre o sistema "
-                          "digestório para tornar o aprendizado mais acessível e divertido. Nossa meta é ajudar estudantes a "
-                          "entender melhor o funcionamento do sistema digestório com perguntas interativas e conteúdo "
-                          "educativo. Aproveite o jogo e aprenda de forma divertida!",
-                          style: TextStyle(
-                              color: Colors.white), // Cor do texto do conteúdo
-                        ),
-                        actions: [
-                          TextButton(
-                            style: ButtonStyle(
-                              overlayColor:
-                                  WidgetStateProperty.resolveWith<Color?>(
-                                (Set<WidgetState> states) {
-                                  if (states.contains(WidgetState.hovered)) {
-                                    return const Color.fromARGB(
-                                            255, 220, 15, 75)
-                                        .withOpacity(0.2); // Efeito hover
-                                  }
-                                  return null; // Usar o mesmo fundo padrão
-                                },
-                              ),
-                              foregroundColor:
-                                  WidgetStateProperty.resolveWith<Color?>(
-                                (Set<WidgetState> states) {
-                                  if (states.contains(WidgetState.hovered)) {
-                                    return const Color.fromRGBO(
-                                        255, 215, 90, 1);
-                                  }
-                                  return const Color.fromARGB(255, 220, 15, 75);
-                                },
-                              ),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Fechar'),
+              Flexible(
+                child: GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          backgroundColor: const Color.fromARGB(
+                              255, 75, 75, 75), // Cor de fundo da interface
+                          title: const Text(
+                            "Sobre nós",
+                            style: TextStyle(
+                                color: Colors.white), // Cor do texto do título
                           ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: const MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: Text(
-                    "Sobre nós",
-                    style: TextStyle(
-                      color: Colors.white, // Cor do texto "Sobre nós"
-                      fontSize: 14,
+                          content: const Text(
+                            "Somos alunos de Ciência da Computação e criamos este jogo didático sobre o sistema "
+                            "digestório para tornar o aprendizado mais acessível e divertido. Nossa meta é ajudar estudantes a "
+                            "entender melhor o funcionamento do sistema digestório com perguntas interativas e conteúdo "
+                            "educativo. Aproveite o jogo e aprenda de forma divertida!",
+                            style: TextStyle(
+                                color: Colors.white), // Cor do texto do conteúdo
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Fechar'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: const FittedBox(
+                    fit: BoxFit.scaleDown, // Shrink the text if needed
+                    child: Text(
+                      "Sobre nós",
+                      style: TextStyle(
+                        color: Colors.white, // Cor do texto "Sobre nós"
+                        fontSize: 14,
+                      ),
                     ),
                   ),
                 ),
               ),
 
               // Imagens centralizadas no meio da BottomAppBar
-              const Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('assets/images/logo_piaget.png'),
-                    height: 700,
-                    width: 150,
-                  ),
-                  SizedBox(width: 15), // Espaçamento entre as imagens
-                  Image(
-                    image: AssetImage('assets/images/logo-IMT.png'),
-                    height: 700,
-                    width: 150,
-                  ),
-                ],
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: Image.asset(
+                        'assets/images/logo_piaget.png',
+                        fit: BoxFit.contain,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                    ),
+                    const SizedBox(width: 15), // Espaçamento entre as imagens
+                    Flexible(
+                      child: Image.asset(
+                        'assets/images/logo-IMT.png',
+                        fit: BoxFit.contain,
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-
-              // Placeholder para ocupar o espaço no final e equilibrar a centralização
-              const SizedBox(width: 50),
             ],
           ),
         ),
